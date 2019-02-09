12905e8bf502a9eb73fdf618232b0c2548d59643
quietQuiet <j.lietunovas@gmail.com>
2019-02-09 15:24:17 +0100

    add correct user log-in return a JWT

--- auth/routes.js
+++ auth/routes.js
@@ -13,7 +13,35 @@ router.post('/tokens', (req,res) => {
             message: `Please supply a valid email and password`
         })
     } else {
-        
+        User
+        .findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(entity => {
+            if(!entity) {
+                res.status(400).send({
+                    message: "User with that email adress doesnt exist"
+                })
+            }
+            if(bcrypt.compareSync(req.body.password, entity.password)) {
+                res.send({
+                    token: toJWT({userID: entity.id}),
+                })
+            }
+            else {
+                res.status(400).send({
+                    message: `Log-in details were incorrect!`
+                })
+            }
+        })
+        .catch(err=>{
+            console.err
+            res.status(500).send({
+                message: 'Something went wrong'
+            })
+        })
     }
 })
 
