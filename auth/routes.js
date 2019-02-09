0e7e567a8e29ce8e4508415321b05bf10f281c90
quietQuiet <j.lietunovas@gmail.com>
2019-02-09 19:46:03 +0100

    add working middleware after fixing a type-o :(

--- auth/routes.js
+++ auth/routes.js
@@ -3,7 +3,7 @@ const {toJWT, toData} = require('./jwt')
 const User = require('../users/model')
 const router = new Router()
 const bcrypt = require('bcrypt')
-const auth =  
+const auth =  require('./middleware')
 
 router.post('/tokens', (req,res) => {
     const email = req.body.email;
@@ -47,27 +47,14 @@ router.post('/tokens', (req,res) => {
 
 // added a secret endpoint route for the JWT authorization
 // since in the HW requirements it wasnt specified where to send the Authorization header :p
-router.get('/secret-endpoint', (req, res) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ')
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-    try {
-        const data = toData(auth[1])
-        res.send({
-          message: 'Thanks for visiting the secret endpoint.',
-          data
-        })
-      }
-      catch(error) {
-        res.status(400).send({
-          message: `Error ${error.name}: ${error.message}`,
-        })
-      }
-    }
-    else {
-      res.status(401).send({
-        message: 'Please supply some valid credentials'
-      })
-    }
+// MiddleWare c
+router.get('/secret-endpoint', auth, (req, res) => {
+    res.send({
+        message: `Thanks for visiting the secret endpoint ${req.user.email}.`
+    })
 })
 
+
+
 module.exports = router
+
